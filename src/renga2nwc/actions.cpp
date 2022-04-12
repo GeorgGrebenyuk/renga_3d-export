e2142cb270d9e2d1407ebfc71f61cc3c3c793a26
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-12 18:57:40 +0300

    Create class (as general for actions with model and Navisworks)

---
+++ src/renga2nwc/actions.cpp
@@ -0,0 +1,61 @@
+#include "actions.h"
+int actions::start() {
+	// Initialise low-level API first. 
+	LiNwcApiErrorInitialise();
+
+	// Then initialise the rest of the API. 
+	switch (LiNwcApiInitialise())
+	{
+	case LI_NWC_API_OK:
+		test_process();
+		break;
+	case LI_NWC_API_NOT_LICENSED:
+		return 1;
+
+	case LI_NWC_API_INTERNAL_ERROR:
+	default:
+		return 1;
+	}
+	LiNwcApiTerminate();
+	return 0;
+}
+void actions::test_process() {
+	std::string dir_path = "E:\\Temp\\";
+	GUID uniq_id;
+	CoCreateGuid(&uniq_id);
+	std::string  uniq_id2 = std::to_string(uniq_id.Data1);
+	std::string all_temp_path = dir_path + uniq_id2 + ".nwc";
+	std::wstring all_temp_path_w(std::begin(all_temp_path), std::end(all_temp_path));
+	LtWideString wfilename = all_temp_path_w.c_str();
+
+	LtNwcScene scene;
+	LtNwcGeometry geom;
+	LtNwcGeometryStream stream;
+
+
+	//create scene and geometry
+	scene = LiNwcSceneCreate();
+	geom = LiNwcGeometryCreate();
+
+	//open geometry stream, draw a triangle, close stream
+	stream = LiNwcGeometryOpenStream(geom);
+	LiNwcGeometryStreamBegin(stream, 0);    LiNwcGeometryStreamTriangleVertex(stream, 1, 0, 0);
+	LiNwcGeometryStreamTriangleVertex(stream, 2, 0, 10);
+	LiNwcGeometryStreamTriangleVertex(stream, 3, 10, 10);
+
+	//********texture********
+	/*LcNwcPresenterMaterial* mat = create_texture(L"TestLayout.bmp");
+	LiNwcNodeAddAttribute(geom, *mat);
+	delete mat;*/
+
+	LiNwcGeometryStreamEnd(stream);    LiNwcGeometryCloseStream(geom, stream);
+
+	//add the geometry to the scene and cleanup geom
+	LiNwcSceneAddNode(scene, geom);
+	LiNwcGeometryDestroy(geom);
+
+	//write out the NWC file
+	LiNwcSceneWriteCacheEx(scene, wfilename, wfilename, 0, 0);
+
+	LiNwcSceneDestroy(scene);
+}
---
+++ src/renga2nwc/actions.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include<string>
+#include "nwcreate/LiNwcAll.h"
+#include"Renga_import.h"
+class actions
+{
+	int start();
+	void test_process();
+};
+
