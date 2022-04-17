a274774492fccd8de7d52c69abfedb261638c3c0
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-17 23:09:23 +0300

    Update renga_debug.cpp

--- src/renga_debug/renga_debug.cpp
+++ src/renga_debug/renga_debug.cpp
@@ -6,11 +6,11 @@
 
 int main()
 {
-    std::string current_path = "C:\\Program Files\\Renga\\Samples\\Cottage.rnp";
+    std::string current_path = "E:\\Temp\\Cottage.rnp"; //"C:\\Program Files\\Renga\\Samples\\Cottage.rnp"
 
     const char* new_path = current_path.c_str();
 
-    default_start(new_path);
+    default_start(new_path, 0);
 
     std::cout << "End!\n";
     return 0;
