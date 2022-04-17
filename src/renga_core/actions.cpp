7aa9e3fabd5b9fed1f64d1fbaf169929df4912b5
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-17 23:07:51 +0300

    Updates
    1. Add checking filepath and selecting method of exporting
    2. Add objects collection

--- src/renga_core/actions.cpp
+++ src/renga_core/actions.cpp
@@ -1,15 +1,19 @@
 #include "actions.h"
 #include "renga_core.h"
+#include <regex>
+
+#include"navis.h"
 
 
-void default_start(const char* path_to_project) {
-	renga_data test_class(path_to_project);
+void default_start(const char* path_to_project, int type_conversion) {
+	renga_data test_class(path_to_project, type_conversion);
 
-	std::cout << test_class.r_project->FilePath << std::endl;
+	//std::cout << test_class.r_project->FilePath << std::endl;
 
 }
-renga_data::renga_data(const char* project_filepath)
+renga_data::renga_data(const char* project_filepath, int type_conversion)
 {
+	this->type = type_conversion;
 	::CoInitialize(nullptr);
 	Renga::IApplicationPtr renga_current_app = Renga::CreateApplication(CLSCTX_LOCAL_SERVER);
 	renga_current_app->PutVisible(VARIANT_TRUE);
@@ -19,9 +23,38 @@ renga_data::renga_data(const char* project_filepath)
 	this->r_project = pProject;
 	this->r_app = renga_current_app;
 
-	auto file_path = this->r_project->FilePath;
-	//renga_current_app->Quit();
-}
-void renga_data::Quit() {
+	//Get file path and set new file name with target extension
+	std::wstring current_project_path(pProject->FilePath, SysStringLen(pProject->FilePath));
+	std::string current_project_path_str(current_project_path.begin(), current_project_path.end());
 
+	GUID uniq_id;
+	CoCreateGuid(&uniq_id);
+	std::string  uniq_id_str = std::to_string(uniq_id.Data1);
+	std::string extension = "";
+	if (this->type == 0) extension = ".nwc";
+	std::string uuid_and_ext = uniq_id_str + extension;
+	std::string renga_ext = ".rnp";
+
+	current_project_path_str.replace(current_project_path_str.find(renga_ext), renga_ext.length(), uuid_and_ext);
+	std::wstring new_path(current_project_path_str.begin(), current_project_path_str.end());
+	this->file_export_path = new_path;
+
+	//Getting geometry, properties, materials ...
+	Renga::IDataExporterPtr pDataExporter = pProject->GetDataExporter();
+	Renga::IExportedObject3DCollectionPtr objects_collection3d = pDataExporter->GetObjects3D();
+	
+	//Renga::IGridWithMaterialCollectionPtr objects_grids = pDataExporter->GetGrids();
+
+	std::vector<renga_object> objects;
+	for (int couter_object = 0; couter_object < objects_collection3d->GetCount(); couter_object++)
+	{
+		Renga::IExportedObject3DPtr pObject = objects_collection3d->Get(couter_object);
+		objects.push_back(renga_object(pObject));
+	}
+	switch (this->type) {
+
+	case 0:
+		navis start_conv(&objects, this->file_export_path);
+		break;
+	}
 }
--- src/renga_core/actions.h
+++ src/renga_core/actions.h
@@ -3,13 +3,31 @@
 #include "string"
 #include "Renga_import.h"
 #include <iostream>
+#include <vector>
+#include <list>
 
 class renga_data {
 public:
-	Renga::IProject *r_project;
-	Renga::IApplication* r_app;
+	Renga::IProjectPtr r_project;
+	Renga::IApplicationPtr r_app;
 
-	renga_data(const char* project_filepath);
-	std::string file_path;
-	void Quit();
+	renga_data(const char* project_filepath, int type_conversion);
+	std::wstring file_export_path;
+	//std::vector<renga_object> objects;
+	int type;
+};
+class renga_object {
+public:
+	GUID obj_type;
+	//geometry
+	std::list<Renga::FloatPoint3D> points_list; //all points at object
+	std::list<std::list<unsigned int>> triangles_definition;//all triangles at object
+	std::list<unsigned int> grid_definition; //indexes in triangles_definition
+	std::list<unsigned int> mesh_definition; //indexes in grid_definition
+	std::list<unsigned int> meshes_definition; //meshes at object
+	//properties ...
+	//material ...
+	// 
+	//class constructor
+	renga_object(Renga::IExportedObject3DPtr internal_object);
 };
