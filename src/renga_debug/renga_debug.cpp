f0bfa77702334094fc12c5cef5b03e81f3db6413
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-27 00:53:15 +0300

    Update progect

--- src/renga_debug/renga_debug.cpp
+++ src/renga_debug/renga_debug.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    std::string current_path = "E:\\DataTest\\Renga\\MetalFramePavilon.rnp"; //"ttage.rnp" E:\\Temp\\BridgeEx.rnp"
+    std::string current_path = "E:\\DataTest\\Renga\\SummerSchool_IS.rnp"; //"ttage.rnp" E:\\Temp\\BridgeEx.rnp"
 
     const char* new_path = current_path.c_str();
 
