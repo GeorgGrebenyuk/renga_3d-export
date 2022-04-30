40d20588fb37af8a4fc53de7198dfcc3d313a1cd
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-01 01:16:10 +0300

    Update navis.cpp
    Add debugging part

--- src/renga_core/navis.cpp
+++ src/renga_core/navis.cpp
@@ -37,8 +37,8 @@ void renga_data::navis_export() {
 	Renga::IExportedObject3DCollectionPtr objects_collection3d = pDataExporter->GetObjects3D();
 	Renga::IModelObjectCollectionPtr objects_collection = this->r_project->GetModel()->GetObjects();
 	
-	std::cout << "objects_collection count" << objects_collection->GetCount() << std::endl;
-	std::cout << "objects_collection3d count" << objects_collection3d->GetCount() << std::endl;
+	std::cout << "objects_collection count = " << objects_collection->GetCount() << std::endl;
+	std::cout << "objects_collection3d count = " << objects_collection3d->GetCount() << std::endl;
 
 	//Getting levels
 	std::list<Renga::IModelObjectPtr> m_levels;
@@ -54,7 +54,7 @@ void renga_data::navis_export() {
 	m_levels.sort(tools::compare_levels);
 	
 	//Create containers for objects on level and as other group -- objects without level's reference
-	std::list<int> non_level_objects;
+	std::list<Renga::IExportedObject3DPtr> non_level_objects;
 	std::list<level_objects> level2objects;
 
 	std::map<int, Renga::IModelObjectPtr> id2level;
@@ -65,7 +65,7 @@ void renga_data::navis_export() {
 	std::cout << "end level work" << std::endl;
 	tools::level2ids(this->r_project, &level2objects, &non_level_objects);
 	std::cout << "end sort objects on levels" << std::endl;
-
+	int counter_objects = 0;
 	for (level_objects one_group : level2objects)
 	{
 		Renga::IModelObjectPtr level_object = id2level[one_group.level_model_id];
@@ -76,11 +76,11 @@ void renga_data::navis_export() {
 		levels_objects.SetName(level_instanse->LevelName);
 		levels_objects.SetLayer(TRUE);
 
-		int counter_objects = 0;
-		for (int counter_model_exporting_id : one_group.ids)
+		
+		for (Renga::IExportedObject3DPtr obj : one_group.objects)
 		{
-			std::cout << counter_objects << std::endl;
-			navis_object::navis_object(this->r_project, &this->projects_offset, &levels_objects, counter_model_exporting_id);
+			std::cout << counter_objects << " " << std::endl;
+			navis_object::navis_object(this->r_project, &this->projects_offset, &levels_objects, obj);
 			counter_objects++;
 		}
 		scene.AddNode(levels_objects);
@@ -89,9 +89,10 @@ void renga_data::navis_export() {
 	LcNwcGroup non_levels_objects;
 	non_levels_objects.SetName(L"Models objects");
 	non_levels_objects.SetLayer(TRUE);
-	for (int counter_model_exporting_id : non_level_objects)
+	for (Renga::IExportedObject3DPtr obj : non_level_objects)
 	{
-		navis_object::navis_object(this->r_project, &this->projects_offset, &non_levels_objects, counter_model_exporting_id);
+		std::cout << counter_objects << " " << std::endl;
+		navis_object::navis_object(this->r_project, &this->projects_offset, &non_levels_objects, obj);
 	}
 	scene.AddNode(non_levels_objects);
 		
