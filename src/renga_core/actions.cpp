f408d61b448c3f35748ae7818a20b63d2a4c1f7b
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-30 20:57:09 +0300

    Update actions.cpp
    Add method to getting project's internal offset parameters

--- src/renga_core/actions.cpp
+++ src/renga_core/actions.cpp
@@ -18,6 +18,9 @@ renga_data::renga_data(const char* project_filepath, int type_conversion)
 	this->r_app->OpenProject(bstr_t(project_filepath));
 	this->r_project = this->r_app->GetProject();
 
+	this->get_transform_parameters();
+	//this->projects_offset = tools::get_transform_parameters(this->r_project);
+
 	//tools::renga_project = this->r_project;
 	//Get file path and set new file name with target extension
 	std::wstring current_project_path(this->r_project->FilePath, SysStringLen(this->r_project->FilePath));
@@ -41,4 +44,48 @@ renga_data::renga_data(const char* project_filepath, int type_conversion)
 		this->navis_init();
 		break;
 	}
+}
+void renga_data::get_transform_parameters()
+{
+	Renga::IBuildingInfoPtr building_object = this->r_project->GetBuildingInfo();
+	Renga::IPropertyContainerPtr props_usual = building_object->GetProperties();
+	
+	bstr_t offset_x = "IfcLocationX";
+	bool is_x_correct = false;
+	double offset_x_value = 0.0;
+	bstr_t offset_y = "IfcLocationY";
+	bool is_y_correct = false;
+	double offset_y_value = 0.0;
+	bstr_t offset_z = "IfcLocationZ";
+	bool is_z_correct = false;
+	double offset_z_value = 0.0;
+	bstr_t angle = "IfcDirectionPrecession";
+	bool angle_correct = false;
+	double angle_value = 0.0;
+
+	//Renga::IGuidCollectionPtr props_ids = props_usual->GetIds();
+	//for (int counter_property = 0; counter_property < props_ids->GetCount(); counter_property++)
+	//{
+	//	GUID id = props_ids->Get(counter_property);
+	//	Renga::IPropertyPtr one_prop = props_usual->Get(id);
+	//	if (one_prop->Name == offset_x && one_prop->GetType() == Renga::PropertyType::PropertyType_Double) {
+	//		offset_x_value = one_prop->GetDoubleValue();
+	//		is_x_correct = true;
+	//	}
+	//	else if (one_prop->Name == offset_y && one_prop->GetType() == Renga::PropertyType::PropertyType_Double) {
+	//		offset_y_value = one_prop->GetDoubleValue();
+	//		is_y_correct = true;
+	//	}
+	//	else if (one_prop->Name == offset_z && one_prop->GetType() == Renga::PropertyType::PropertyType_Double) {
+	//		offset_z_value = one_prop->GetDoubleValue();
+	//		is_z_correct = true;
+	//	}
+	//	else if (one_prop->Name == angle && one_prop->GetType() == Renga::PropertyType::PropertyType_Angle) {
+	//		angle_value = one_prop->GetAngleValue(Renga::AngleUnit::AngleUnit_Degrees);
+	//		angle_correct = true;
+	//	}
+	//}
+	
+	std::vector<double> find_transform_parameters{ offset_x_value,offset_y_value,offset_z_value,angle_value };
+	this->projects_offset = find_transform_parameters;
 }
