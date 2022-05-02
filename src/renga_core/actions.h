640e5e931b8db9452cc2324c6e1fa30e2c940f6a
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-02 14:23:18 +0300

    Update actions.h
    Change logic for levels-objects

--- src/renga_core/actions.h
+++ src/renga_core/actions.h
@@ -68,7 +68,7 @@ public:
 		assert(pLevel2 != nullptr);
 		double l1_h = pLevel1->GetElevation();
 		double l2_h = pLevel2->GetElevation();
-		return l1_h < l2_h;
+		return l1_h <= l2_h;
 	}
 	//static std::vector <double> get_transform_parameters(Renga::IProjectPtr link_project);
 	static std::vector <double> get_transformed_coords(double x, double y, double z, std::vector<double>  current_params)
@@ -79,7 +79,8 @@ public:
 		double new_z = current_params.at(2) + z;
 		return { new_x,new_y,new_z };
 	}
-	static void level2ids (Renga::IProjectPtr link_project, std::list<level_objects>* level2object, std::list<Renga::IExportedObject3DPtr> *non_level_objects);
+	static void level2ids (Renga::IProjectPtr link_project, std::list<level_objects>* level2object,
+		std::list<Renga::IExportedObject3DPtr> *non_level_objects, std::list<Renga::IModelObjectPtr>* m_levels);
 //private:
 	//static std::vector<double> transform_parameters;
 };
@@ -95,4 +96,5 @@ private:
 	void create_geometry(LcNwcGroup* object_defenition);
 	void getting_properties(LcNwcGroup* object_defenition);
 	void getting_color(LcNwcGroup* object_defenition);
+	void getting_color_grid(LcNwcGeometry* geometry_definition, GUID* object_type, int *grid_type);
 };
