996f731b836cc947b0d0dfac5d47c10d7cbdff81
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-05-02 15:30:42 +0300

    updates

--- src/renga_debug/renga_debug.cpp
+++ src/renga_debug/renga_debug.cpp
@@ -7,7 +7,7 @@
 int main()
 {
     //TestHouse_Offset problem with getting props in IBuildingInfoPtr
-    std::string current_path = "E:\\DataTest\\Renga\\Renga_IS_1.rnp"; //"ttage.rnp" E:\\Temp\\BridgeEx.rnp"
+    std::string current_path = "E:\\DataTest\\Renga\\TestBeam.rnp"; //"ttage.rnp" E:\\Temp\\BridgeEx.rnp"
     //
     const char* new_path = current_path.c_str();
 
