1bb1904015eec69e5bf4fbe8b9a311c4a47db273
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-21 22:37:11 +0300

    Add condition for NWC export

--- src/loader/ConfigureTool.cs
+++ src/loader/ConfigureTool.cs
@@ -52,7 +52,16 @@ namespace loader
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (radioButton1.Checked)
+            {
+                radioButton_grids.Enabled = false;
+                checkBox_use_max_triangles.Enabled = false;
+            }
+            else
+            {
+                radioButton_grids.Enabled = true;
+                checkBox_use_max_triangles.Enabled = true;
+            }
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
