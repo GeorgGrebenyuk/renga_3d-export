1dd6e23877ea3143babd967f5ca608c5d24f477f
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-22 00:35:12 +0300

    Update general_data.hpp
    Add objects on and non on level

--- src/exporter/general_data.hpp
+++ src/exporter/general_data.hpp
@@ -1,4 +1,53 @@
 //variables
+#include "Renga/ObjectTypes.h"
 static int max_triangles = 0;
 static bool use_recalc = false;
 static bool use_hidded = true;
+
+const std::list<GUID> c_levelTreeTypes =
+{
+  Renga::ObjectTypes::Wall,
+  Renga::ObjectTypes::Column,
+  Renga::ObjectTypes::Floor,
+  Renga::ObjectTypes::Opening,
+  Renga::ObjectTypes::Roof,
+  Renga::ObjectTypes::Beam,
+  Renga::ObjectTypes::Stair,
+  Renga::ObjectTypes::Ramp,
+  Renga::ObjectTypes::Window,
+  Renga::ObjectTypes::Door,
+  Renga::ObjectTypes::Railing,
+  Renga::ObjectTypes::Room,
+  Renga::ObjectTypes::IsolatedFoundation,
+  Renga::ObjectTypes::WallFoundation,
+  Renga::ObjectTypes::AssemblyInstance,
+  Renga::ObjectTypes::Element,
+  Renga::ObjectTypes::Plate,
+  Renga::ObjectTypes::RoutePoint,
+  Renga::ObjectTypes::Equipment,
+  Renga::ObjectTypes::PlumbingFixture,
+  Renga::ObjectTypes::MechanicalEquipment,
+  Renga::ObjectTypes::Line3D,
+  Renga::ObjectTypes::Hatch,
+  Renga::ObjectTypes::TextShape,
+  Renga::ObjectTypes::Rebar
+};
+
+const std::list<GUID> c_nonLevelTreeTypes
+{
+  Renga::ObjectTypes::PipeAccessory,
+  Renga::ObjectTypes::PipeFitting,
+  Renga::ObjectTypes::Pipe,
+  Renga::ObjectTypes::Duct,
+  Renga::ObjectTypes::DuctAccessory,
+  Renga::ObjectTypes::DuctFitting,
+  Renga::ObjectTypes::LineElectricalCircuit,
+  Renga::ObjectTypes::LightFixture,
+  Renga::ObjectTypes::ElectricDistributionBoard,
+  Renga::ObjectTypes::WiringAccessory,
+  Renga::ObjectTypes::Route,
+  Renga::ObjectTypes::Axis,
+  Renga::ObjectTypes::Elevation,
+  Renga::ObjectTypes::Section,
+  Renga::ObjectTypes::Dimension,
+};
