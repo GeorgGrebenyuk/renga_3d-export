35d660caa22299177773d76918eeaea5056b98ec
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-21 22:36:05 +0300

    Update handler_function.cpp
    Add starter for export-classes

--- src/exporter/handler_function.cpp
+++ src/exporter/handler_function.cpp
@@ -3,6 +3,7 @@
 #include "general_data.hpp"
 #include "Renga\CreateApplication.hpp"
 #include "renga_data.hpp"
+#include "navisworks.hpp"
 
 void run_exporter(bool use_hidden, int use_max_triangles,
 	bool recalc, int export_mode, int export_format) 
@@ -14,11 +15,13 @@ void run_exporter(bool use_hidden, int use_max_triangles,
 		use_recalc = recalc;
 		use_hidded = use_hidden;
 
-		renga_app->UI->ShowMessageBox(Renga::MessageIcon_Info, "Note", "OK!");
-
-		//renga_data project_info(renga_app, export_mode);
+		renga_data project_info(renga_app, export_mode);
 		//run other classes for export needing data
-		
+
+		if (export_format == 0) new navisworks(&project_info);
+		//else if (export_format == 1) new fbx (&project_info);
+
+		renga_app->UI->ShowMessageBox(Renga::MessageIcon_Info, "Сообщение", "Экспорт окончен!");
 	}
 	else
 	{
