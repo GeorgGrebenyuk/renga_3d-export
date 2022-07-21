088f0b4ef7944067cd0da321ef5c69da896fcb5c
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-21 22:36:29 +0300

    Create skeleton for Navisworks-export class

---
+++ src/exporter/navisworks.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "navisworks.hpp"
+#include "LiNwcAll.h"
+navisworks::navisworks(renga_data* data)
+{
+
+}
---
+++ src/exporter/navisworks.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "renga_data.hpp"
+#pragma comment(linker, "/manifestdependency:\"name='nwcreate' version='1.0.0.0' type='win32'\"")
+class navisworks
+{
+public: 
+	navisworks(renga_data* data);
+private:
+	void start();
+};
+
