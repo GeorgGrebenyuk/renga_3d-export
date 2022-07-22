2e7e97591c1dfff4f2e980d50806b2af76ea8de7
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-22 23:49:41 +0300

    Edit parse and sort objects logic

--- src/exporter/navisworks.cpp
+++ src/exporter/navisworks.cpp
@@ -17,24 +17,38 @@ navisworks::navisworks(renga_data* data)
 	case LI_NWC_API_NOT_LICENSED:
 		printf("\n\nNot Licensed\n");
 		status = 1;
+		break;
 	case LI_NWC_API_INTERNAL_ERROR:
 	default:
 		printf("\n\nInternal Error\n");
 		status = 2;
+		break;
 	}
 }
-void navisworks::parse_level_objects(std::map<const char*, std::vector<int>>* data, LcNwcGroup* to_record)
+void navisworks::parse_level_objects(std::map<const char*, std::vector<int>> data, LcNwcGroup* to_record)
 {
-	for (auto type2objects : *data)
+	//bool is_any_geometry_all = false;
+	for (std::pair<const char*, std::vector<int>> type2objects : data)
 	{
 		const char* group_name = type2objects.first;
+		std::vector<int> objects = type2objects.second;
 		bstr_t group_name_str = group_name;
 		LcNwcGroup object_type_group;
+		//bool is_any_geometry = false;
 		object_type_group.SetName(group_name_str);
 		object_type_group.SetLayer(TRUE);
 
-		for (int model_object_id : type2objects.second)
+		for (int model_object_id : objects)
 		{
+			//std::map<int, object_3d_info>::iterator check_exist_geometry;
+			//check_exist_geometry = this->project_data->objects_3d_info.find(model_object_id);
+			//if (check_exist_geometry != this->project_data->objects_3d_info.end())
+			//{
+
+			//}
+
+			//is_any_geometry = true;
+			//is_any_geometry_all = true;
 			object_3d_info info = this->project_data->objects_3d_info[model_object_id];
 			LcNwcGroup object_nwc;
 			bstr_t object_name_str = info.object_name;
@@ -54,7 +68,7 @@ void navisworks::parse_level_objects(std::map<const char*, std::vector<int>>* da
 				body_material.SetDiffuseColor(sub_color[0] / 255.0, sub_color[1] / 255.0, sub_color[2] / 255.0);
 				body_material.SetAmbientColor(sub_color[0] / 255.0, sub_color[1] / 255.0, sub_color[2] / 255.0);
 				body_material.SetName(sub_material_name);
-				body_material.SetTransparency(sub_color[3] / 100.0);
+				body_material.SetTransparency(1 - sub_color[3]/255.0);
 				grid_data.AddAttribute(body_material);
 
 				std::vector<Renga::FloatPoint3D> points;
@@ -79,16 +93,19 @@ void navisworks::parse_level_objects(std::map<const char*, std::vector<int>>* da
 				object_nwc.AddNode(grid_data);
 			}
 			object_type_group.AddNode(object_nwc);
+
+			
 		}
 		(*to_record).AddNode(object_type_group);
 	}
+	//return is_any_geometry_all;
 
 }
 void navisworks::start() {
 
 	LcNwcScene scene;
-	bstr_t file_path = this->project_data->project_path;
-	LtWideString wfilename = file_path;
+	//bstr_t file_path = this->project_data->project_path;
+	LtWideString wfilename = this->project_data->project_path.c_str();
 
 	//Getting geometry, properties, materials ...
 	for (auto level_info : this->project_data->levels_objects)
@@ -98,13 +115,16 @@ void navisworks::start() {
 		LcNwcGroup levels_objects;
 		levels_objects.SetName(level_object->LevelName);
 		levels_objects.SetLayer(TRUE);
-		parse_level_objects(&level_info.second, &levels_objects);
+		parse_level_objects(level_info.second, &levels_objects);
 		scene.AddNode(levels_objects);
 	}
 	LcNwcGroup non_levels_objects;
-	parse_level_objects(&this->project_data->non_levels_objects, &non_levels_objects);
+	non_levels_objects.SetName(L"OtherObjects");
+	non_levels_objects.SetLayer(TRUE);
+	parse_level_objects((this->project_data->non_levels_objects), &non_levels_objects);
 	scene.AddNode(non_levels_objects);
 	
+	int ii = 0;
 	scene.WriteCache(L"", wfilename, LI_NWC_NO_PROGRESS_CALLBACKS, LI_NWC_NO_USER_DATA);
 
 }
--- src/exporter/navisworks.hpp
+++ src/exporter/navisworks.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include "renga_data.hpp"
 #include "general_data.hpp"
-#pragma comment(linker, "/manifestdependency:\"name='nwcreate' version='1.0.0.0' type='win32'\"")
+//#pragma comment(linker, "/manifestdependency:\"name='nwcreate' version='1.0.0.0' type='win32'\"")
 #include "nwcreate\LiNwcAll.h"
 class navisworks
 {
 public: 
 	navisworks(renga_data* data);
 private:
-	void parse_level_objects(std::map<const char*, std::vector<int>>* data, LcNwcGroup* to_record);
+	void parse_level_objects(std::map<const char*, std::vector<int>> data, LcNwcGroup* to_record);
 	void start();
 	renga_data* project_data;
 };
